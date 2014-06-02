d1ca5bf313faa66dea2aecb3d95f0ce4dd9e1e76
MagicProG <vengrig@mail.ru>
2014-06-02 08:12:10 +0800

    Update OperationTests.java

--- OperationTests.java
+++ OperationTests.java
@@ -1,13 +1,7 @@
 import junit.framework.Assert;
 import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: User
- * Date: 26.05.14
- * Time: 7:59
- * To change this template use File | Settings | File Templates.
- */
+
 public class OperationTests {
 	@Test
 	public void testAddition(){
